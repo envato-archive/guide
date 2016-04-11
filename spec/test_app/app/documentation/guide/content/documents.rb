b1eee26994a2d3ad654038924f9761b52e3b8374
Jordan Lewis <jordan.lewis@envato.com>
2016-04-12 09:59:28 +1000

    Add markdown kitchen sink example

--- spec/test_app/app/documentation/guide/content/documents.rb
+++ spec/test_app/app/documentation/guide/content/documents.rb
@@ -3,5 +3,6 @@ module Guide
     contains :public
     contains :unpublished, visibility: :unpublished
     contains :restricted, visibility: :restricted
+    contains :supported_formats
   end
 end
