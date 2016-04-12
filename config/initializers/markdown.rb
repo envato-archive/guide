8874f8e933ed358e4a765491b98cd7c977959be5
Zubin Henner <zubin@users.noreply.github.com>
2016-04-12 11:23:58 +1000

    Use github-markup to render markdown

--- config/initializers/markdown.rb
+++ config/initializers/markdown.rb
@@ -1,10 +1,11 @@
+require 'github/markup'
 require 'rdiscount'
 
 class ActionView::Template
   module Handlers
     class Markdown
       def self.call(template)
-        markdown = RDiscount.new(template.source).to_html
+        markdown = GitHub::Markup.render('README.markdown', template.source)
         "%{<div class='#{Guide.configuration.markdown_wrapper_class}'>#{markdown}</div>}.html_safe"
       end
     end
